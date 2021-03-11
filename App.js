d5387a18315120768ae228d04a1e37ff14d98494
Zye-thecoder <71854197+Zye-thecoder@users.noreply.github.com>
2021-03-11 18:48:00 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hhhhhhhhhhhhhhhhhhgkhlfhfkhrkhkthfhfhfhfhfhfjfjjfjdhgsshgsgsgehhdhdhdhfddhdhdhzsggyjub7gfyhs9oegh7e97oa8g9hyuhogyvaphoghyugh8hafh94873y8073y9apty09hjgzp9hgzs9gzsughgh0r98s7ghr7gh07gh0s78g</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
